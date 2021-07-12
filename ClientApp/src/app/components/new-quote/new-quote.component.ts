7de7aba525b9e3f0b59f7d157138134dc2a1031e
Josh <jak_dev@outlook.com>
2021-07-12 17:22:25 -0400

    Added form validation to quote form

--- ClientApp/src/app/components/new-quote/new-quote.component.ts
+++ ClientApp/src/app/components/new-quote/new-quote.component.ts
@@ -9,27 +9,37 @@ import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@ang
 export class NewQuoteComponent implements OnInit {
   quoteForm: FormGroup;
   items: FormArray;
+  submitted: boolean = false;
+  generateQuoteValue: boolean = true;
+
+  numberRegEx = /\-?\d*\.?\d{1,2}/;
+  phoneRegex = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/;
+  currencyRegex = /^[0-9]+(\.[0-9]{1,2})?$/;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.quoteForm = this.formBuilder.group({
-      customerName: '',
-      email: '',
-      phone: '',
-      quoteNum: '',
+      customerName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(this.phoneRegex)]],
+      quoteNum: ['', [Validators.required, Validators.pattern(this.numberRegEx)]],
+      generateQuoteNum: true,
       items: this.formBuilder.array([this.createItem()])
     });
+  }
 
+  onGenerateQuoteNum(e): void {
+    this.generateQuoteValue = e.target.checked;
   }
 
   createItem(): FormGroup {
     return this.formBuilder.group({
-      quantity: 0,
-      sku: '',
-      description: '',
-      unitCost: 0.00,
-      subTotal: 0.00,
+      quantity: ['', [Validators.required, Validators.pattern(this.numberRegEx)]],
+      sku: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
+      unitCost: ['', [Validators.required, Validators.pattern(this.currencyRegex)]],
+      subTotal: ['', [Validators.required, Validators.pattern(this.currencyRegex)]],
     });
   }
 
@@ -45,4 +55,36 @@ export class NewQuoteComponent implements OnInit {
       this.items.removeAt(numberOfItems);
     }
   }
+
+  removeAllItems(): void {
+    try{
+      let numberOfItems = this.items.length-1;
+      for(let i = 0; i<numberOfItems+1; i++){
+        this.removeItem();
+      }
+    }
+    catch{}
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.quoteForm.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // stop here if form is invalid
+      if (this.quoteForm.invalid) {
+          return;
+      }
+
+      // display form values on success
+      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.quoteForm.value, null, 4));
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.quoteForm.reset();
+      this.removeAllItems();
+  }
+
 }
